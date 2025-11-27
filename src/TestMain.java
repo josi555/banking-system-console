04ce6b931586362c8cd0e7f8645711fcef074283
Yosief  Gebrewold <152223898+josi555@users.noreply.github.com>
2025-11-26 18:22:51 -0800

    Update TestMain.java

--- src/TestMain.java
+++ src/TestMain.java
@@ -5,12 +5,12 @@ public class TestMain {
 
         Bank bank = new Bank();
 
-        // create a few accounts
+     
         bank.createAccount("1001", "Yosief", 500.0);
         bank.createAccount("1002", "Sara", 1000.0);
         bank.createAccount("1001", "Duplicate", 300.0); // should fail
 
-        // check that we can access an account
+     
         Account acc = bank.getAccount("1001");
         System.out.println("\nAccount 1001 belongs to: " + acc.getHolderName());
         System.out.println("Balance: " + acc.getBalance());
